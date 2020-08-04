ca39bbe23359cf9f85882ac083d3709ccf811944
LuanVieiradeAraujo <50274276+LuanVieiradeAraujo@users.noreply.github.com>
2020-08-03 18:31:13 -0700

    Java package corrections

---
+++ WebIIExercicio2/src/java/ufpr/ex2/Usuario.java
@@ -0,0 +1,35 @@
+package ufpr.ex2;
+
+public class Usuario {
+    private String nome;
+    private String login;
+    private String senha;
+    
+    public Usuario(){
+
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }    
+}

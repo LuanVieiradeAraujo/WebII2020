6f0b877230fe603ea5db03036d9c2f46823a7dfc
LuanVieiradeAraujo <50274276+LuanVieiradeAraujo@users.noreply.github.com>
2020-08-25 16:37:08 -0700

    Mysql dependency fix

--- WebIIExercicio7/src/java/com/ufpr/tads/web2/dao/ConnectionFactory.java
+++ WebIIExercicio7/src/java/com/ufpr/tads/web2/dao/ConnectionFactory.java
@@ -11,7 +11,7 @@ public class ConnectionFactory {
     public Connection getConnection() throws SQLException { 
             try {
                 Class.forName("com.mysql.jdbc.Driver");
-                return DriverManager.getConnection("jdbc:mysql://localhost:3306/tarefa2?useSSL=false", "root", "");
+                return DriverManager.getConnection("jdbc:mysql://localhost:3306/tarefa2?useSSL=false", "root", "Ipsec@01");
             } 
             catch (SQLException | ClassNotFoundException ex) { 
                 System.out.println("Erro ao conectar ao banco: " + ex);

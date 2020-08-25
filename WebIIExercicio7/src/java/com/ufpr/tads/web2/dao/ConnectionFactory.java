14607b29f72aff0009063afd72856e8c056f76b8
LuanVieiradeAraujo <50274276+LuanVieiradeAraujo@users.noreply.github.com>
2020-08-25 16:38:45 -0700

    Update ConnectionFactory.java

--- WebIIExercicio7/src/java/com/ufpr/tads/web2/dao/ConnectionFactory.java
+++ WebIIExercicio7/src/java/com/ufpr/tads/web2/dao/ConnectionFactory.java
@@ -11,7 +11,8 @@ public class ConnectionFactory {
     public Connection getConnection() throws SQLException { 
             try {
                 Class.forName("com.mysql.jdbc.Driver");
-                return DriverManager.getConnection("jdbc:mysql://localhost:3306/tarefa2?useSSL=false", "root", "Ipsec@01");
+                // return DriverManager.getConnection("jdbc:mysql://localhost:3306/tarefa2?useSSL=false", "root", "Ipsec@01"); <- Carolina
+                return DriverManager.getConnection("jdbc:mysql://localhost:3306/tarefa2?useSSL=false", "root", "Ipsec@01"); // <- Luan
             } 
             catch (SQLException | ClassNotFoundException ex) { 
                 System.out.println("Erro ao conectar ao banco: " + ex);
